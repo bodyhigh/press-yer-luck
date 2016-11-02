6b5ca12a124f83aca38d07ee7b2960102d9e2cda
Craig Moreno <craig@bodyhigh.com>
2016-11-02 01:38:14 -0700

    Added getUser() and updated tests

--- server/data/user.js
+++ server/data/user.js
@@ -1,20 +1,21 @@
 var q = require('q');
 
-var removeUser = function(username, redisClient) {
+var removeUser = function(user, redisClient) {
     return q.promise(function(resolve, reject, notify) {
-        redisClient.srem('users', username, function(err) {
-            if (err) {
-                reject();
-            }
-            resolve();
-        });
+        redisClient.multi()
+            .del(user.username)
+            .srem('users', user.username)
+            .exec(function(err) {
+                if (err) {
+                    reject(err);
+                }
+                resolve();
+            });
     });
 };
 
 var setUser = function(user, expire, redisClient) {
     return q.promise(function (resolve, reject, notify) {
-        console.log('!!!!!!!!!!!!!!!');
-        console.log(user.username);
         redisClient.multi()
             .hmset(user.username, 'totalPoints', '0', 'totalTurns', '0')
             .expire(user.username, expire)
@@ -30,9 +31,26 @@ var setUser = function(user, expire, redisClient) {
     });
 };
 
+var getUser = function(username, redisClient) {
+    return q.promise(function(resolve, reject, notify) {
+        redisClient.hgetall(username, function(err, res) {
+            if (err === null) {
+                if (res === null) {
+                    resolve(null);
+                } else {
+                    resolve({username: username, totalPoints: parseInt(res.totalPoints), totalTurns: parseInt(res.totalTurns)});
+                }
+            } else {
+                reject(err);
+            }
+        });
+    });
+};
+
 var user = {
     removeUser: removeUser,
-    setUser: setUser
+    setUser: setUser,
+    getUser: getUser
 };
 
 module.exports = user;
--- test/data.user.js
+++ test/data.user.js
@@ -4,14 +4,14 @@ var redisClient = require('./../server/database/redis.js');
 
 describe('User Repo', function() {
     beforeEach('Flush Redis', function(done) {
-        redisClient.flushdb();
+        // redisClient.flushdb();
         done();
     });
 
     describe('setUser()', function() {
         it('setUser enters a user record [Users] set', function(done) {
             var user = {username: 'tycho.dobbs'};
-            var userResult = repo.user.setUser(user.username, 7200, redisClient);
+            var userResult = repo.user.setUser(user, 7200, redisClient);
             userResult.done(function() {
                 redisClient.sismember('users', user.username, function(err, res) {
                     assert(res, true);
@@ -20,18 +20,54 @@ describe('User Repo', function() {
             });
         });
     });
-    //
-    // describe('removeUser()', function() {
-    //     it('removes a user from [Users] set', function(done) {
-    //         var user = {username: 'bob.dobbs'};
-    //         repo.user.setUser(user.username, 7200, redisClient).done(function() {
-    //             repo.user.removeUser(user.username, redisClient).done(function(err, res) {
-    //                 redisClient.sismember('users', user.username, function(err, res) {
-    //                     assert.equal(res, false);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //     });
-    // });
+
+    describe('removeUser()', function() {
+        it('removes a user from [Users] set', function(done) {
+            var user = {username: 'bob.dobbs'};
+            repo.user.setUser(user, 7200, redisClient).done(function() {
+                repo.user.removeUser(user, redisClient).done(function(err, res) {
+                    // Verify the username has been removed from the Set [users]
+                    redisClient.sismember('users', user.username, function(err, res) {
+                        assert.equal(res, false);
+
+                        // Verify the Hash [bob.dobbs] has been deleted
+                        redisClient.keys(user.username, function(err, res) {
+                            assert.equal(res, false);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    describe('getUser()', function() {
+        it('User object is returned if it exists as a Hash', function(done) {
+            var seedUser = {username: 'calibos.dobbs'};
+            // Seed a fake user to retrieve
+            repo.user.setUser(seedUser, 7200, redisClient).done(function() {
+                // Search for our seedUser
+                var user = repo.user.getUser(seedUser.username, redisClient);
+                user.then(function(res) {
+                    assert.equal(res.username, seedUser.username);
+                    assert.equal(res.totalPoints, 0);
+                    assert.equal(res.totalTurns, 0);
+                    done();
+                });
+            }, function(err) {
+                throw err;
+            });
+        });
+
+        it('Returns null result if user is not found as a Hash', function(done) {
+            var username = 'hadrian.dobbs';
+            var user = repo.user.getUser(username, redisClient);
+
+            user.then(function(res) {
+                assert(res === null);
+                done();
+            });
+        });
+    });
+
 });
